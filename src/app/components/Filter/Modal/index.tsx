f7d1c403e6c56705b43a4731b29af5d7d324a7bc
Fahad <fahad04598@gmal.com>
2024-06-12 17:50:54 +0530

    Update custom filter ui

--- src/app/components/Filter/Modal/index.tsx
+++ src/app/components/Filter/Modal/index.tsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
-import ScheduledDate from "../ScheduledDate"
-import People from "../People"
+import ScheduledDate from "../ScheduledDate";
+import People from "../People";
 import ServicesProducts from "../ServicesProducts";
-import SidePanel from "../Modal/SidePanel"
+import SidePanel from "../Modal/SidePanel";
 
-const Modal = () => {
+const Modal = ({ isModalOpen }: { isModalOpen: boolean }) => {
   const [selectedTab, setSelectedTab] = useState("Scheduled Date");
 
   return (
     <div
-      className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50"
+      className={`${isModalOpen ? "pointer-events-auto visible translate-y-[5px] opacity-100" : "pointer-events-none invisible translate-y-[-10px] opacity-0"} absolute left-0 top-[100%] z-10 rounded-[6px] border duration-300 xl:duration-200 ease-in-out xl:h-[400px] xl:w-[612px] xl:flex-[unset] xl:bg-[unset]`}
       role="dialog"
       aria-modal="true"
       aria-labelledby="filter-dialog-title"
+      tabIndex={isModalOpen ? 0 : -1}
     >
-      <div className="mx-auto flex h-[400px] w-full max-w-[95%] flex-col rounded-[6px] bg-white shadow-lg sm:max-w-[612px] sm:flex-[unset]">
+      <div className="mx-auto flex h-[400px] w-full flex-col rounded-[6px] bg-white shadow-lg sm:max-w-[612px] sm:flex-[unset]">
         <div className="flex grow flex-col sm:h-[348px] sm:grow-[unset] sm:flex-row">
-          <SidePanel selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+          <SidePanel
+            selectedTab={selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
           <Content selectedTab={selectedTab} />
         </div>
         <div className="flex items-center justify-end gap-x-[16px] border-t px-[16px] py-[8px] text-[14px] font-[500] leading-[24px]">
@@ -41,11 +45,5 @@ const Content = ({ selectedTab }: any) => {
     </div>
   );
 };
-
-
-
-
-
-
 
 export default Modal;
