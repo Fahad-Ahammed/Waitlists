676c23d18ae3444abf60bf703eed6c2d84ac4ad0
Fahad <fahad04598@gmal.com>
2024-06-13 04:37:32 +0530

    Update Search component, for more custom styling

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -4,19 +4,33 @@ import { RootState } from "@/redux/store";
 
 type SearchBarProps = {
   onSearch: (query: string) => void;
+  classNames: SearchStyle;
+  placeholder: string;
 };
 
-const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+export type SearchStyle = {
+  form: string;
+  iconContainer: string;
+  icon: string;
+  iconPath: string;
+  input: string;
+};
+
+const SearchBar: React.FC<SearchBarProps> = ({
+  onSearch,
+  classNames,
+  placeholder,
+}) => {
   const { currentTabSlug } = useSelector((state: RootState) => state.waitlist);
 
   const [query, setQuery] = useState("");
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
+    onSearch(event.target.value);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    onSearch(query);
   };
 
   useEffect(() => {
@@ -28,21 +42,20 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
       onSubmit={handleSubmit}
       role="search"
       aria-label="Search clients"
-      className="relative rounded-sm bg-white px-[35px] py-[5px] shadow-md"
+      className={classNames.form}
     >
-      <div className="pointer-events-none absolute inset-y-0 left-[16px] flex items-center pr-3">
+      <div className={classNames.iconContainer}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="12"
-          height="12"
           viewBox="0 0 12 12"
           fill="none"
+          className={classNames.icon}
         >
           <path
             d="M10.5 10.5L8.34998 8.34998M9.5 5.5C9.5 7.70914 7.70914 9.5 5.5 9.5C3.29086 9.5 1.5 7.70914 1.5 5.5C1.5 3.29086 3.29086 1.5 5.5 1.5C7.70914 1.5 9.5 3.29086 9.5 5.5Z"
-            stroke="#64748B"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            className={classNames.iconPath}
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </div>
@@ -54,8 +67,8 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
         id="search"
         value={query}
         onChange={handleChange}
-        placeholder="Search Clients"
-        className="w-full text-[12px] font-[500] leading-[20px] text-[#374151] outline-none placeholder:text-[#94A3B8]"
+        placeholder={placeholder}
+        className={classNames.input}
       />
     </form>
   );
